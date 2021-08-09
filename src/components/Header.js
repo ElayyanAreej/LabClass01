539b567a333725b13496dae9a92f9e4786fd47d5
ElayyanAreej <areej.hossein@gmail.com>
2021-08-09 17:05:17 +0300

    finished lab 02

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import React from 'react';
 class Header extends React.Component {
     render() {
         return (
-   <h1>Gallery Horrens</h1>
+   <h1>Gallery Horned Beasts</h1>
         )
     }
 }
